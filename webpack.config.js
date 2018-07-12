6a4800a1c2592b27a82ef768125457f85d20090e
Lukas Bischof <polinderis@gmail.com>
2018-07-12 23:46:35 +0200

    Upgrade webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
+  mode: 'production',
   entry: ['./src/UABanner.js', './src/UABanner.css'],
   output: {
     filename: 'UABanner.min.js',
